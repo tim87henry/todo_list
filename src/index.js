b3d5ea904b45310e7be0789c07f3a996b11a3627
Timothy <tim87henry@gmail.com>
2021-02-20 21:27:16 +1100

    Printed list of projects with links to display Todos

--- src/index.js
+++ src/index.js
@@ -18,18 +18,34 @@ const content = document.getElementById("content");
 
 const navbar = document.createElement('div');
 navbar.id="navbar";
-navbar.innerHTML="Project List"+"<br>"+showProjects();
+navbar.innerHTML="Project List"+"<br>"+"<br>";
+const new_project=document.createElement("button");
+new_project.innerHTML="Add project";
+navbar.appendChild(new_project)
+const project_list = document.createElement('div');
+project_list.id="project_list";
+navbar.appendChild(project_list);
 content.appendChild(navbar);
+showProjects();
 
 function showProjects() {
-    let list="";
     for (let i=0;i<listOfProjects.length;i++) {
-        list=list+listOfProjects[i].name+"<br>";
+        let project_link=document.createElement('div');
+        project_link.innerText=listOfProjects[i].name;
+        project_link.className="project_link";
+        project_list.appendChild(project_link);
+        let line_break=document.createElement('div');
+        line_break.innerHTML="";
+        project_list.appendChild(line_break);
     }
-    return list;
 }
 
-const todoData = document.createElement('div');
-todoData.id="todo_data";
-todoData.innerHTML="Here are the list of Todos : "+"<br>"+test_project.listTodos();
-content.appendChild(todoData);
+let project_links=document.getElementsByClassName("project_link");
+for (let i=0;i<project_links.length;i++) {
+    project_links[i].addEventListener('click', function (e) {
+        const todoData = document.createElement('div');
+        todoData.id="todo_data";
+        todoData.innerHTML="Here are the list of Todos : "+"<br>"+listOfProjects[i].listTodos();
+        content.appendChild(todoData);
+    });
+}
